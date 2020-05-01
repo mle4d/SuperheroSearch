a65e2d9d3ff1703ee37f48571b3d20e24ff21c69
emily <mle4d@me.com>
2020-05-01 09:45:00 -0700

    testing one superhero on play click

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector('.play').addEventListener('click', function() {
+  document.querySelector('.block1').innerHTML = '<img class="superhero" src="assets/superheros/Buzz.png">';
+  window.body
+});
