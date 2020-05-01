acee9d6d8d7b6b420627dfd11c1c8a661054e537
emily <mle4d@me.com>
2020-05-01 10:14:00 -0700

    change play image when playing

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ document.querySelector('.play').addEventListener('click', function() {
   document.querySelector('.block1').innerHTML = '<img class="superhero" src="assets/superheros/Buzz.png">';
   document.querySelector('.level').innerHTML = 'Level 1';
   document.querySelector('.middle').style.opacity = '1';
+  document.querySelector('.play').style.background = 'url(assets/go.png) center no-repeat';
+  document.querySelector('.play').style.backgroundSize = '80%';
   window.body
 });
