32bd6e8812c5b8bf79d800d8e4b97a1235b8dca2
emily <mle4d@me.com>
2020-05-01 10:04:31 -0700

    add in level on click & show middle

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.querySelector('.play').addEventListener('click', function() {
   document.querySelector('.block1').innerHTML = '<img class="superhero" src="assets/superheros/Buzz.png">';
+  document.querySelector('.level').innerHTML = 'Level 1';
+  document.querySelector('.middle').style.opacity = '1';
   window.body
 });
