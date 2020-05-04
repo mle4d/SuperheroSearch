8e6e56663d0a662a6d93c21cac99dad07b49c40a
emily <mle4d@me.com>
2020-05-03 20:17:31 -0700

    continue round 2 js

--- roundtwo.js
+++ roundtwo.js
@@ -1,22 +1,20 @@
 document.querySelector('.round2').addEventListener('click', function() {
-  document.querySelector('.superhero1').style.display = 'flex';
-  document.querySelector('.superhero1').classList.add('batman');
-  document.querySelector('.superhero3').classList.add('greenlantern');
-  document.querySelector('.superhero4').classList.add('wolverine');
-  document.querySelector('.superhero8').classList.add('greenranger');
+  document.querySelector('.superhero2').classList.add('batman');
+  document.querySelector('.superhero5').classList.add('shera');
+  document.querySelector('.superhero6').classList.add('elastigirl');
+  document.querySelector('.superhero7').classList.add('greenlantern');
+  document.querySelector('.superhero8').classList.add('gekko');
   document.querySelector('.level').innerHTML = 'Round 2';
   document.querySelector('.results').innerHTML = 'Difficulty Level: Easy';
   document.querySelector('.middle').style.opacity = '1';
   document.querySelector('.round2').classList.toggle('go');
   setTimeout(function() {
     document.querySelector('.results').innerHTML = `you found ${score} out of 5`;
-    document.querySelector('.round2').style.display = 'none';
-    document.querySelector('.round3').style.display = 'flex';
-    document.querySelector('.superhero2').style.background = 'none';
-    document.querySelector('.superhero5').style.background = 'none';
-    document.querySelector('.superhero6').style.background = 'none';
-    document.querySelector('.superhero7').style.background = 'none';
-    document.querySelector('.superhero9').style.background = 'none';
+    document.querySelector('.superhero2').classList.remove('batman');
+  document.querySelector('.superhero5').classList.remove('shera');
+  document.querySelector('.superhero6').classList.remove('elastigirl');
+  document.querySelector('.superhero7').classList.remove('greenlantern');
+  document.querySelector('.superhero8').classList.remove('gekko');
 }, 30000);
   window.body
   document.querySelector('.batman').addEventListener('click', function() {
@@ -24,5 +22,25 @@ document.querySelector('.round2').addEventListener('click', function() {
     const testsound = new Audio ('assets/sounds/batmansound.mp3');
     testsound.play();
     window.body
-  });
+    document.querySelector('.shera').addEventListener('click', function() {
+      document.querySelector('.shera').style.display = 'none';
+      const sherasound = new Audio ('assets/sounds/sherasound.mp3');
+      sherasound.play();
+    document.querySelector('.elastigirl').addEventListener('click', function() {
+      document.querySelector('.elastigirl').style.display = 'none';
+      const elastigirlsound = new Audio ('assets/sounds/elastigirlsound.mp3');
+      elastigirlsound.play();
+      document.querySelector('.greenlantern').addEventListener('click', function() {
+      document.querySelector('.greenlantern').style.display = 'none';
+      const greenlanternsound = new Audio ('assets/sounds/greenlanternsound.mp3');
+      greenlanternsound.play();
+      document.querySelector('.gekko').addEventListener('click', function() {
+        document.querySelector('.gekko').style.display = 'none';
+        const gekkosound = new Audio ('assets/sounds/gekkosound.mp3');
+        gekkosound.play();
+      });
+        });
+          });
+            });
+          });
 });
