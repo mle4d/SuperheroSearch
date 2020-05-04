38fd77bd0f159c122472c67c759a2ee81e9f2353
emily <mle4d@me.com>
2020-05-04 14:29:58 -0700

    tweak round 2 js

--- roundtwo.js
+++ roundtwo.js
@@ -7,36 +7,38 @@ document.querySelector('.round2').addEventListener('click', function() {
   document.querySelector('.level').innerHTML = 'Round 2';
   document.querySelector('.results').innerHTML = 'Difficulty Level: Easy';
   document.querySelector('.middle').style.opacity = '1';
-  document.querySelector('.round2').classList.toggle('go');
+  document.querySelector('.round2').style.visibility = 'hidden';
+  document.querySelector('#press').style.visibility = 'hidden';
+
   document.querySelector('.batman').addEventListener('click', function() {
-    document.querySelector('.batman').style.display = 'none';
-    const testsound = new Audio ('assets/sounds/batmansound.mp3');
-    testsound.play();
-    window.body
+    document.querySelector('.superhero2').classList.remove('batman');
+    const batmansound = new Audio ('assets/sounds/batmansound.mp3');
+    batmansound.play();
+  });
     document.querySelector('.shera').addEventListener('click', function() {
-      document.querySelector('.shera').style.display = 'none';
-      const sherasound = new Audio ('assets/sounds/sherasound.mp3');
-      sherasound.play();
-    document.querySelector('.elastigirl').addEventListener('click', function() {
-      document.querySelector('.elastigirl').style.display = 'none';
-      const elastigirlsound = new Audio ('assets/sounds/elastigirlsound.wav');
-      elastigirlsound.play();
-      document.querySelector('.greenlantern').addEventListener('click', function() {
-      document.querySelector('.greenlantern').style.display = 'none';
-      const greenlanternsound = new Audio ('assets/sounds/greenlanternsound.mp3');
-      greenlanternsound.play();
-      document.querySelector('.gekko').addEventListener('click', function() {
-        document.querySelector('.gekko').style.display = 'none';
-        const gekkosound = new Audio ('assets/sounds/gekkosound.mp3');
-        gekkosound.play();
-      });
-        });
-          });
-            });
-              });
+    document.querySelector('.superhero5').classList.remove('shera');
+    const sherasound = new Audio ('assets/sounds/sherasound.mp3');
+    sherasound.play();
+  });
+  document.querySelector('.elastigirl').addEventListener('click', function() {
+    document.querySelector('.superhero6').classList.remove('elastigirl');
+    const elastigirlsound = new Audio ('assets/sounds/elastigirlsound.wav');
+    elastigirlsound.play();
+  });
+  document.querySelector('.greenlantern').addEventListener('click', function() {
+    document.querySelector('.superhero7').classList.remove('greenlantern');
+    const greenlanternsound = new Audio ('assets/sounds/greenlanternsound.mp3');
+    greenlanternsound.play();
+  });
+  document.querySelector('.gekko').addEventListener('click', function() {
+    document.querySelector('.superhero9').classList.remove('gekko');
+    const gekkosound = new Audio ('assets/sounds/gekkosound.mp3');
+    gekkosound.play();
+  });
   setTimeout(function() {
     document.querySelector('.round2').style.display = 'none';
     document.querySelector('.round3').style.display = 'flex';
+    document.querySelector('#press').style.visibility = 'visible';
     document.querySelector('.results').innerHTML = `you found ${score} out of 5`;
     document.querySelector('.superhero2').classList.remove('batman');
     document.querySelector('.superhero5').classList.remove('shera');
