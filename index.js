c8d552ce8a7575c293461c8f585584d132819a04
emily <mle4d@me.com>
2020-05-04 17:16:01 -0700

    take round 3 js out

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const scoreBoard = document.querySelector('.score');
 const wholeScore = document.querySelector('.wholescore');
 const superheros = document.querySelectorAll('.superhero');
 let score = 0;
+
 function startWholeGame() {
   wholeScore.textContent = 0;
   timeUp = false;
@@ -23,40 +24,12 @@ function bonk(e) {
 
 superheros.forEach(superhero => superhero.addEventListener('click', bonk));
 
-// document.querySelector('.round3').addEventListener('click', function() {
-//   document.querySelector('.superhero1').style.background = 'url(assets/superheros/wonderwoman.png) center no-repeat';
-//   document.querySelector('.superhero3').style.background = 'url(assets/superheros/superman.png) center no-repeat';
-//   document.querySelector('.superhero4').style.background = 'url(assets/superheros/Elastigirl.png) center no-repeat';
-//   document.querySelector('.superhero5').style.background = 'url(assets/superheros/deadpool.png) center no-repeat';
-//   document.querySelector('.superhero7').style.background = 'url(assets/superheros/superman.png) center no-repeat';
-//   document.querySelector('.superhero8').style.background = 'url(assets/superheros/blackpanther.png) center no-repeat';
-//   document.querySelector('.level').innerHTML = 'Round 3';
-//   document.querySelector('.results').innerHTML = 'Difficulty Level: intermediate';
-//   document.querySelector('.middle').style.opacity = '1';
-//   document.querySelector('.round3').classList.toggle('go');
-//   setTimeout(function() {
-//     document.querySelector('.results').innerHTML = `you found ${score} out of 6`;
-//     document.querySelector('.round3').style.display = 'none';
-//     document.querySelector('.round4').style.display = 'flex';
-//     document.querySelector('.superhero1').style.background = 'none';
-//     document.querySelector('.superhero3').style.background = 'none';
-//     document.querySelector('.superhero4').style.background = 'none';
-//     document.querySelector('.superhero5').style.background = 'none';
-//     document.querySelector('.superhero7').style.background = 'none';
-//     document.querySelector('.superhero8').style.background = 'none';
-// }, 30000);
-//   window.body
-// });
-
-document.querySelector('.superhero2').addEventListener('click', function() {
-  document.querySelector('.superhero2').style.display = 'none';
-  window.body
-});
 
 let countdown;
 const timerDisplay = document.querySelector('.timer');
 const buttons = document.querySelectorAll('[data-time]');
 const buzzer = new Audio ('assets/sounds/buzzersound.m4a');
+
 
 function timer(seconds) {
   clearInterval(countdown);
@@ -69,7 +42,6 @@ function timer(seconds) {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     if(secondsLeft < 6) {
       timerDisplay.style.color ='#f23c27';
-
       buzzer.play()
     }
     if(secondsLeft < 0) {
@@ -97,4 +69,5 @@ function startTimer() {
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
+
 
